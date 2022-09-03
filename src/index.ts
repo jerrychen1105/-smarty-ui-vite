a497a566881412efe867e6e1875640842e8800ac
jerry <398256957@qq.com>
2022-09-03 15:13:31 +0800

    day01

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+const s: string = 'Hello Typescript'
+console.log(s)
+import { createApp } from "vue";
+
+// import SButton from "./button";
+
+// createApp(SButton).mount("#app");
+
+// import SFCButton from "./SFCButton.vue";
+
+// createApp(SFCButton)
+// .mount("#app");
+import JSXButton from './JSXButton'
+createApp(JSXButton)
+.mount("#app");
